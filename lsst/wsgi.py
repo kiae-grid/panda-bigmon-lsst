kiae-grid/panda-bigmon-atlas

WSGI script: bring some sanity
 - avoid hardcoding Python version;

 - allow to specify multiple directories inside WSGI_PATH
   with usual Unix separator (':');

 - don't use default values for virtual environment and
   WSGI path: first one is always requred, so we bail out
   if it isn't specified; the second one is optional, so
   we will just notify about its absence;

 - rearrange blocks to group same-purpose code;

 - remove some old commented cruft.

Signed-off-by: Eygene Ryabinkin <rea@grid.kiae.ru>

atlas/wsgi.py
@@ -15,36 +15,37 @@ import sys
 import site
 from os.path import join, pardir, abspath, dirname, split
 
-### dummy settings settings_bigpandamon file with VIRTUALENV_PATH, WSGI_PATH
-baseSettingsPath = '/data/bigpandamon_settings'
-sys.path.append(baseSettingsPath)
 
-#virtualenvPath = '/data/virtualenv/django1.6.1__python2.6.6__jedimon'
-virtualenvPath = '/data/virtualenv/django1.6.1__python2.6.6__atlas'
-path = virtualenvPath + '/pythonpath'
+virtualenvPath = None
 try:
-#    from settings_bigpandamon_jedimon import VIRTUALENV_PATH
-#    from settings_bigpandamon_jedimon import WSGI_PATH
     from settings_bigpandamon_atlas import VIRTUALENV_PATH
-    from settings_bigpandamon_atlas import WSGI_PATH
     virtualenvPath = VIRTUALENV_PATH
-    path = WSGI_PATH
-except:
-    print "Something went wrong with import of WSGI_PATH from settings."
-    print "Staying with default path: %s" % path
-
+except Exception, e:
+    print "Export of VIRTUALENV_PATH from settings failed: %s" % (e)
+    print "Can't work without virtualenv, exiting."
+    sys.exit(1)
 # Add the site-packages of the chosen virtualenv to work with
-site.addsitedir(virtualenvPath + '/lib/python2.6/site-packages')
+site.addsitedir(virtualenvPath + '/lib/python%s/site-packages' % \
+  (".".join(map(lambda x: str(x), sys.version_info[0:2]))))
 
-# Add the app's directory to the PYTHONPATH
-sys.path.append(path)
-sys.path.append(path + '/pythonpath')
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bigpandamon.settings")
-#os.environ["DJANGO_SETTINGS_MODULE"] = "atlas.settings"
+wsgi_path = None
+try:
+    from settings_bigpandamon_atlas import WSGI_PATH
+    wsgi_path = WSGI_PATH
+except Exception, e:
+    print "Export of WSGI_PATH from settings failed: %s" % (e)
+    print "Will not extend Python path."
+
+if wsgi_path:
+    sys.path.extend(wsgi_path.split(':'))
+
 
 # django settings module
 DJANGO_SETTINGS_MODULE = '%s.%s' % (split(abspath(dirname(__file__)))[1], 'settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = DJANGO_SETTINGS_MODULE
+
+
 # pythonpath dirs
 PYTHONPATH = [
     join(dirname(__file__), pardir),
@@ -56,19 +57,9 @@ for p in PYTHONPATH:
         sys.path.insert(0, p)
 
 
-os.environ['DJANGO_SETTINGS_MODULE'] = DJANGO_SETTINGS_MODULE
-
-
-# Activate your virtual env
+# Activate virtual env
 activate_env = os.path.expanduser(virtualenvPath + '/bin/activate_this.py')
 execfile(activate_env, dict(__file__=activate_env))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-## Apply WSGI middleware here.
-## from helloworld.wsgi import HelloWorldApplication
-## application = HelloWorldApplication(application)
-#
-
-

kiae-grid/panda-bigmon-lsst

lsst/wsgi.py
@@ -15,37 +15,33 @@ import sys
 import site
 from os.path import join, pardir, abspath, dirname, split
 
-### dummy settings settings_bigpandamon file with VIRTUALENV_PATH, WSGI_PATH
-baseSettingsPath = '/data/bigpandamon_settings'
-sys.path.append(baseSettingsPath)
-
-#virtualenvPath = '/data/virtualenv/django1.6.1__python2.6.6__jedimon'
-#virtualenvPath = '/data/virtualenv/django1.6.1__python2.6.6__atlas'
-virtualenvPath = '/data/wenaus/virtualenv/twrpm'
-path = virtualenvPath + '/pythonpath'
+virtualenvPath = None
 try:
-#    from settings_bigpandamon_jedimon import VIRTUALENV_PATH
-#    from settings_bigpandamon_jedimon import WSGI_PATH
     from settings_bigpandamon_twrpm import VIRTUALENV_PATH
-    from settings_bigpandamon_twrpm import WSGI_PATH
     virtualenvPath = VIRTUALENV_PATH
-    path = WSGI_PATH
-except:
-    print "Something went wrong with import of WSGI_PATH from settings."
-    print "Staying with default path: %s" % path
+except Exception, e:
+    print "Export of VIRTUALENV_PATH from settings failed: %s" % (e)
+    print "Can't work without virtualenv, exiting."
+    sys.exit(1)
 
 # Add the site-packages of the chosen virtualenv to work with
-site.addsitedir(virtualenvPath + '/lib/python2.6/site-packages')
+site.addsitedir(virtualenvPath + '/lib/python%s/site-packages' % \
+  (".".join(map(lambda x: str(x), sys.version_info[0:2]))))
 
-# Add the app's directory to the PYTHONPATH
-sys.path.append(path)
-sys.path.append(path + '/pythonpath')
+wsgi_path = None
+try:
+    from settings_bigpandamon_twrpm import WSGI_PATH
+    wsgi_path = WSGI_PATH
+except Exception, e:
+    print "Export of WSGI_PATH from settings failed: %s" % (e)
+    print "Will not extend Python path."
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bigpandamon.settings")
-#os.environ["DJANGO_SETTINGS_MODULE"] = "atlas.settings"
+if wsgi_path:
+    sys.path.extend(wsgi_path.split(':'))
 
 # django settings module
 DJANGO_SETTINGS_MODULE = '%s.%s' % (split(abspath(dirname(__file__)))[1], 'settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = DJANGO_SETTINGS_MODULE
 # pythonpath dirs
 PYTHONPATH = [
     join(dirname(__file__), pardir),
@@ -57,19 +53,9 @@ for p in PYTHONPATH:
         sys.path.insert(0, p)
 
 
-os.environ['DJANGO_SETTINGS_MODULE'] = DJANGO_SETTINGS_MODULE
-
-
-# Activate your virtual env
+# Activate virtual env
 activate_env = os.path.expanduser(virtualenvPath + '/bin/activate_this.py')
 execfile(activate_env, dict(__file__=activate_env))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-## Apply WSGI middleware here.
-## from helloworld.wsgi import HelloWorldApplication
-## application = HelloWorldApplication(application)
-#
-
-